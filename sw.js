57b1d7fa5f7a3b58cf6a96c5831250929b3af860
exocowboy <62403228+bergeronK@users.noreply.github.com>
2026-06-09 14:50:19 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = 'twilight-v2';
+const CACHE = 'twilight-v3';
 const ASSETS = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
